6628e5cbffdf7d6f10a08ae6ccb8f44403de0df2
musa.coce <musa.coce@gsb.gov.tr>
2023-08-25 16:47:10 +0300

    yeni proje başlangıcı için entityler silindi.

--- src/Api/Core/MsCc.Backend.Api.Domain/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MsCc.Backend.Api.Domain.Models;
-public class Category
-{
-    public int CategoryId { get; set; }
-    public string CategoryName { get; set; }
-    public bool IsActive { get; set; }
-
-    public virtual ICollection<Product> Product { get; set; }
-}
--- src/Api/Core/MsCc.Backend.Api.Domain/Models/EmailConfirmation.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MsCc.Backend.Api.Domain.Models;
-public class EmailConfirmation
-{
-    public string OldEmailAddress { get; set; }
-    public string NewEmailAdress { get; set; }
-}
